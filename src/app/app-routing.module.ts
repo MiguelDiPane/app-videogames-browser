bc95d7edea16e1a784e4f0082ebc2c8af9de9d4a
MiguelDiPane <82734749+MiguelDiPane@users.noreply.github.com>
2022-03-02 21:42:42 -0300

    agregar servicios rutas y modelos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Rutas
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'search/:game-search',
+    component: HomeComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  BASE_URL: 'https://rawg-app-videogames-browser.p.rapidapi.com'
 };
